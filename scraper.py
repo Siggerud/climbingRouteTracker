f7204729ced47c7ee8c60751fca1426c0881cc57
Siggerud <christian.siggerud@gmail.com>
2023-03-09 21:52:33 +0100

    files modified

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from threading import Thread
+from requests_html import HTMLSession
 
 # TODO: find a way to scrape dynamic pages
 
@@ -8,17 +9,52 @@ from threading import Thread
 class CragScraper:
     def __init__(self):
         self._baseUrl = "https://27crags.com"
-        self._crags = []
+        self._cragInfo = {}
 
-    def _getSoup(self, url):
-        response = requests.get(self._baseUrl + url)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, "html.parser")
+    def getCragRoutesInfo(self, url):
+        soup = self._getSoup(url + "/routelist")
+        cragTags = soup.find_all("td", class_="txt")
+        cragRoutesInfo = []
+        for cragTag in cragTags:
+            name = cragTag.find("a", class_="lfont").text
+            grade, style = cragTag.find("div", class_="visible-xs-block").text.strip().split()[0:2]
+            starTag = cragTag.find("span", class_="stars")
+            fullStars = len(starTag.find_all("div", class_="star full glyphicon glyphicon-star"))
+            halfStars = len(starTag.find_all("div", class_="star half glyphicon glyphicon-star-empty"))
+            numberOfStars = fullStars - 0.5 * halfStars
+            temp = [name, grade, style, numberOfStars]
+            cragRoutesInfo.append(temp)
+
+        return cragRoutesInfo
+
+    def _getSoup(self, url, dynamic=False):
+        if dynamic:
+            session = HTMLSession()
+            response = session.get(self._baseUrl + url)
+            response.html.render()
+            soup = BeautifulSoup(response.html.raw_html, "html.parser")
+        else:
+            response = requests.get(self._baseUrl + url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.content, "html.parser")
 
         return soup
 
-    def _getSoupAfterJavascript(self, url):
-        # skriv selenium kode her
+    def getGradeInfo(self, cragUrl):
+        soup = self._getSoup(cragUrl, dynamic=True)
+        gradeSummary = soup.find("div", class_="crag-summary summary-big")
+        spanTags = gradeSummary.find_all("span", class_="cell")
+
+        gradeSummary = {}
+        for tag in spanTags:
+            numOfRoutes = tag.next.text
+            if numOfRoutes == "":
+                continue
+
+            grade = tag.find("span")["title"]
+            gradeSummary[grade] = numOfRoutes
+
+        return gradeSummary
 
     def getCountriesForContinent(self, continent):
         countries = []
@@ -30,53 +66,59 @@ class CragScraper:
 
     def _getLastPage(self, soup):
         navTag = soup.find("ul", role="navigation")
-        lastPage = int(navTag.find_all("li")[-2].text)
+        if navTag == None: # if only one page
+            lastPage = 1
+        else:
+            lastPage = int(navTag.find_all("li")[-2].text)
 
         return lastPage
 
-    def getCragsForCountry(self, country):
+    def getCragInfoForCountry(self, country):
+        self._cragInfo = {} # reset dict
         soup = self._getSoup("/countries/" + country.lower())
         lastPage = self._getLastPage(soup)
-
         trimmingThreads = []
-        for i in range(0, lastPage +1, 1):
+        for i in range(1, lastPage +1, 1):
             start = i
             end = i+1
-            if start == 0:
-                start = 1
-            trimmingThread = Thread(target=self._threadedGetCragsForCountry, args=(country, start, end))
+            trimmingThread = Thread(target=self._threadedGetCragInfoForCountry, args=(country, start, end))
             trimmingThreads.append(trimmingThread)
             trimmingThread.start()
 
         for thread in trimmingThreads:
             thread.join()
 
-    def _threadedGetCragsForCountry(self, country, start, end):
+        return self._cragInfo
+
+    def _threadedGetCragInfoForCountry(self, country, start, end):
         for i in range(start, end):
-            url = f"/countries/{country}/descending/by/favourite_count/page/{i}"
-            soup = self._getSoupAfterJavascript(url)
+            url = f"/countries/{country}"
+            if i != 1:
+                url += f"/descending/by/favourite_count/page/{i}"
+            soup = self._getSoup(url)
             cragTags = soup.find_all("div", class_="crag-summary")
             for tag in cragTags:
-                info = []
+                cragInfo = []
                 name = tag.find("div", class_="crag-name").text
                 typesOfRoutesTag = tag.find_all("span", class_="route-count")
+                routeInfo = []
                 for elem in typesOfRoutesTag:
+                    temp = []
                     if not elem.is_empty_element:
                         try:
                             typeOfRoute = elem.find("span", class_="name").text.strip()
                         except:
                             continue
                         numberOfRoutes = elem.find("i").next.text.strip()
-                        routeInfo = [typeOfRoute, numberOfRoutes]
-                        info.append(routeInfo)
+                        temp.append(typeOfRoute)
+                        temp.append(numberOfRoutes)
+                        routeInfo.append(temp)
+                cragInfo.append(routeInfo)
+                link = tag.parent.parent["href"]
+                cragInfo.append(link)
 
-                spanTags = tag.find_all("span", class_="cell")
-                for spanTag in spanTags:
-                    numberOfGrade = spanTag.next.text
-                    print(numberOfGrade)
+                self._cragInfo[name] = cragInfo
 
-                info.append(name)
-                self._crags.append(info)
 
 
 
--- trackerGUI.py
+++ trackerGUI.py
@@ -1,45 +1,178 @@
-from tkinter import Tk, ttk, StringVar, Checkbutton, IntVar
+from tkinter import Tk, ttk, StringVar, Checkbutton, Listbox, Variable, Frame, Label, \
+    Toplevel, Button
 from scraper import CragScraper
+from scrapeCleaner import ScrapeCleaner
+
+# TODO: make sure the scraping only gets executed at the start of the program
+# initialize the scraper in the class
 
 class trackerGUI:
     def __init__(self, master):
         self._master = master
-        self._master.geometry("300x300")
+        self._master.geometry("300x400")
         self._master.title("Climbing routes tracker")
+
+        self._cragInfo = []
+        self._scraper = CragScraper()
 
         self._continentVar = StringVar()
         self._continent = ttk.Combobox(self._master, textvariable=self._continentVar, width=10)
         self._continent.place(x=10, y=10)
-
         self._setContinent()
+
+        self._continent.bind("<<ComboboxSelected>>", self._updateCountries)
 
         self._countryVar = StringVar()
         self._country = ttk.Combobox(self._master, textvariable=self._countryVar, width=10)
         self._country.place(x=10, y=40)
-
         self._setCountries()
 
-        self._styleSportVar = IntVar()
+        self._country.bind("<<ComboboxSelected>>", self._updateCrags)
+
+        self._styleSportVar = StringVar()
         self._styleSport = Checkbutton(self._master, text="Sport", variable=self._styleSportVar,
-                                       onvalue=1, offvalue=0)
+                                       onvalue="sport", offvalue="", command=self._getResults)
         self._styleSport.place(x=5, y=70)
 
-        self._styleTradVar = IntVar()
+        self._styleTradVar = StringVar()
         self._styleTrad = Checkbutton(self._master, text="Trad", variable=self._styleTradVar,
-                                       onvalue=1, offvalue=0)
+                                       onvalue="trad", offvalue="", command=self._getResults)
         self._styleTrad.place(x=65, y=70)
 
-        self._styleBoulderVar = IntVar()
+        self._styleBoulderVar = StringVar()
         self._styleBoulder = Checkbutton(self._master, text="Boulder", variable=self._styleBoulderVar,
-                                      onvalue=1, offvalue=0)
+                                      onvalue="boulder", offvalue="", command=self._getResults)
         self._styleBoulder.place(x=125, y=70)
 
-        self._styleDwsVar = IntVar()
+        self._styleDwsVar = StringVar()
         self._styleDws = Checkbutton(self._master, text="DWS", variable=self._styleDwsVar,
-                                      onvalue=1, offvalue=0)
+                                      onvalue="DWS", offvalue="", command=self._getResults)
         self._styleDws.place(x=5, y=95)
 
+        self._resultVar = Variable()
+        self._resultList = Listbox(self._master, listvariable=self._resultVar, height=15)
+        self._resultList.place(x=5, y=125)
 
+        self._scrapeResults()
+        self._getResults()
+
+        self._resultList.bind('<<ListboxSelect>>', self._showCragGradeInfo)
+
+        self._cragGradeInfoFrame = Frame(self._master)
+        self._cragGradeInfoFrame.place(x=150, y=125)
+
+    def _showCragGradeInfo(self, event):
+        if self._resultList.curselection() == ():
+            return
+
+        for widget in self._cragGradeInfoFrame.winfo_children():
+            widget.destroy()
+
+        cragGradeLabel = Label(self._cragGradeInfoFrame, text="Grade")
+        cragGradeLabel.grid(row=0, column=0)
+
+        cragNumberLabel = Label(self._cragGradeInfoFrame, text="Number")
+        cragNumberLabel.grid(row=0, column=1)
+
+        gradeSummary = self._getGradeSummary()
+        rowCount = 1
+        for grade, number in gradeSummary.items():
+            gradeLabel = Label(self._cragGradeInfoFrame, text=grade)
+            gradeLabel.grid(row=rowCount, column=0, sticky="w")
+
+            numberLabel = Label(self._cragGradeInfoFrame, text=number)
+            numberLabel.grid(row=rowCount, column=1, sticky="w")
+
+            rowCount += 1
+
+        markButton = Button(self._cragGradeInfoFrame, text="Mark ascents", bg="springgreen", command=self._makePopup)
+        markButton.grid(row=rowCount, column=0, sticky="w", columnspan=2)
+
+    def _makePopup(self):
+        self._markAscendsWindow = Toplevel(self._master)
+        self._markAscendsWindow.geometry("200x200")
+        self._markAscendsWindow.title("Mark ascents")
+
+        url = self._getCragUrl()
+        cragRoutesInfo = self._scraper.getCragRoutesInfo(url)
+
+        nameTitleLabel = Label(self._markAscendsWindow, text="Route")
+        nameTitleLabel.grid(row=0, column=0, sticky="w")
+
+        gradeTitleLabel = Label(self._markAscendsWindow, text="Grade")
+        gradeTitleLabel.grid(row=0, column=1, sticky="w")
+
+        styleTitleLabel = Label(self._markAscendsWindow, text="Type")
+        styleTitleLabel.grid(row=0, column=2, sticky="w")
+
+        starsTitleLabel = Label(self._markAscendsWindow, text="Stars")
+        starsTitleLabel.grid(row=0, column=3, sticky="w")
+
+        rowCount = 1
+        for routeInfo in cragRoutesInfo:
+            nameLabel = Label(self._markAscendsWindow, text=routeInfo[0])
+            nameLabel.grid(row=rowCount, column=0, sticky="w")
+
+            gradeLabel = Label(self._markAscendsWindow, text=routeInfo[1])
+            gradeLabel.grid(row=rowCount, column=1, sticky="w")
+
+            styleLabel = Label(self._markAscendsWindow, text=routeInfo[2])
+            styleLabel.grid(row=rowCount, column=2, sticky="w")
+
+            stars = str(routeInfo[3])
+            if stars == "0.0":
+                stars = ""
+            else:
+                if stars[-1] == "0":
+                    stars = stars[0]
+
+            starsLabel = Label(self._markAscendsWindow, text=stars)
+            starsLabel.grid(row=rowCount, column=3, sticky="w")
+
+            rowCount += 1
+
+            # TODO: sort in order by grade in scrapeCleaner
+    def _getCragUrl(self):
+        index = self._resultList.curselection()
+        country = self._resultList.get(index)
+        url = self._cragInfo[country][1]
+
+        return url
+    def _getGradeSummary(self):
+        url = self._getCragUrl()
+        gradeSummary = self._scraper.getGradeInfo(url)
+
+        return gradeSummary
+
+    def _updateCrags(self, event):
+        self._scrapeResults()
+        self._getResults()
+
+    def _updateCountries(self, event):
+        self._setCountries()
+        self._scrapeResults()
+        self._getResults()
+
+    def _scrapeResults(self):
+        country = self._countryVar.get()
+        self._cragInfo = self._scraper.getCragInfoForCountry(country)
+
+    def _getResults(self):
+        sport = self._styleSportVar.get()
+        trad = self._styleTradVar.get()
+        boulder = self._styleBoulderVar.get()
+        dws = self._styleDwsVar.get()
+
+        allStyles = [sport, trad, boulder, dws]
+        selectedStyles = [style for style in allStyles if style]
+
+        cleaner = ScrapeCleaner(self._cragInfo)
+        results = cleaner.getCrags(selectedStyles)
+        cragNames = []
+        for cragInfo in results:
+            cragNames.append(cragInfo)
+        cragNames.sort()
+        self._resultVar.set(cragNames)
 
 
     def _setContinent(self):
@@ -47,14 +180,12 @@ class trackerGUI:
         self._continent.current(0)
 
     def _setCountries(self):
-        scraper = CragScraper()
         continentRaw = self._continentVar.get()
-        if len(continentRaw) == 2:
+        if len(continentRaw.split()) == 2:
             continent = "-".join(continentRaw.split())
         else:
             continent = continentRaw
-
-        countries = scraper.getCountriesForContinent(continent)
+        countries = self._scraper.getCountriesForContinent(continent)
         self._country["values"] = countries
         if continent == "Europe":
             index = countries.index("Norway")
